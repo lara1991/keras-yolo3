48220f02d45c70751f6c4120c14bc523468b96fe
lara1991 <32224979+lara1991@users.noreply.github.com>
2021-11-01 10:51:20 +0530

    changed the epochs for training

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ def _main():
                 steps_per_epoch=max(1, num_train//batch_size),
                 validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
                 validation_steps=max(1, num_val//batch_size),
-                epochs=50,
+                epochs=25,
                 initial_epoch=0,
                 callbacks=[logging, checkpoint])
         model.save_weights(log_dir + 'trained_weights_stage_1.h5')
@@ -81,8 +81,8 @@ def _main():
             steps_per_epoch=max(1, num_train//batch_size),
             validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
             validation_steps=max(1, num_val//batch_size),
-            epochs=150,
-            initial_epoch=50,
+            epochs=100,
+            initial_epoch=25,
             callbacks=[logging, checkpoint, reduce_lr]) #callbacks=[logging, checkpoint, reduce_lr, early_stopping])
         model.save_weights(log_dir + 'trained_weights_final.h5')
         model.save(log_dir + 'trained_model_final.h5')
